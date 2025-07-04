f160cec62f47fa1773d26dbbbe4f0e73397a0d58
omardoescode <omardoescode@gmail.com>
2025-07-04 15:07:56 +0300

    docs: auto generation for commit bf3ca64

--- src/util/type_guards.ts
+++ src/util/type_guards.ts
@@ -1,3 +1,3 @@
-export const isString = value => typeof value === 'string';
-export const isNumber = value => typeof value === 'number';
-export const isBoolean = value => typeof value === 'boolean';
+export const isString = (value: unknown) => typeof value === 'string';
+export const isNumber = (value: unknown) => typeof value === 'number';
+export const isBoolean = (value: unknown) => typeof value === 'boolean';
